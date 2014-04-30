46d821f40d3e159101121fff35178f591be382c8
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-04-30 22:23:49 +0200

    initial commit

---
+++ Puppetfile
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+#^syntax detection
+
+forge "http://forge.puppetlabs.com"
+
+mod 'puppetlabs/stdlib'
+mod 'puppetlabs/apt'
+mod 'puppetlabs/java',
+   :git => 'https://github.com/puppetlabs/puppetlabs-java.git'
+mod 'maestrodev/wget'
+mod 'willdurand/nodejs'
+mod 'gini/archive',
+   :git => 'https://github.com/rattermeyer/puppet-archive.git'
+mod 'paulosuzart/gvm'
+mod 'garethr/docker'
+
+mod 'ntp',
+   :git => 'git://github.com/puppetlabs/puppetlabs-ntp.git'
+mod 'devtools',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-devtools.git'
+mod 'javatools',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-javatools.git'
+mod 'jstools',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-jstools.git'
+mod 'puppet',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-puppet.git'
+mod 'user',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-user.git'
+mod 'intellij',
+   :git => 'https://RAT@git.opitz-consulting.de/scm/~rat/public-puppet-module-intellij.git'
+
+# mod 'apt',
+#   :git => 'https://github.com/puppetlabs/puppetlabs-apt.git',
+#   :ref => 'feature/master/dans_refactor'
+
+# mod 'apt',
+#   :git => 'https://github.com/puppetlabs/puppetlabs-apt.git',
+#   :ref => 'feature/master/dans_refactor'
---
+++ manifests/nodes.pp
@@ -0,0 +1,52 @@
+node 'ubuntu-trusty.fritz.box' {
+  include puppet
+  include 'docker'
+  include devtools::core
+  include devtools::desktop
+  include javatools
+  include javatools::apache_tomcat
+  include javatools::apache_maven
+  include javatools::jboss_wildfly
+#  include javatools::intellij
+  include javatools::sts
+  include javatools::squirrel
+  include intellij
+  intellij::plugin { 'AngularJS' :
+	name => 'AngularJS'
+  }
+  intellij::plugin { 'sonar-intellij-plugin' :
+	name => 'sonar-intellij-plugin',
+	url	=> 'http://plugins.jetbrains.com/plugin/download?pr=idea\&updateId=15763'
+  }
+  intellij::plugin { 'lombok-plugin' :
+	name => 'lombok-plugin',
+	url	=> 'http://plugins.jetbrains.com/plugin/download?pr=idea\&updateId=15697'
+  }
+  include jstools
+  include jstools::yeoman
+  include nodejs
+  include user::virtual
+  include user::developers
+  class { 'gvm' :   
+    owner => 'dev',
+    require => Class['user::developers'],
+  }
+  gvm::package { 'grails':
+    version    => '2.3.7',
+    is_default => true,
+    ensure     => present, #default
+    require    => Class['gvm'],
+    timeout    => 600
+  }
+  gvm::package { 'groovy':
+    version    => '2.2.2',
+    is_default => true,
+    ensure     => present, #default
+    require    => Class['gvm'],
+    timeout    => 600
+  }
+  file_line { 'maven_path' :
+    path  => '/home/dev/.bashrc',
+    line  => 'export PATH=${PATH}:/opt/apache-maven/current/bin'
+  }	 
+}
