a378e2370b942be3cfbaf33dd4e4f697959a2508
Richard Attermeyer <richard.attermeyer@gmail.com>
2014-07-09 21:20:32 +0000

    added gitlab profile

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -21,46 +21,7 @@ node 'ci-master' {
   class { '::mysql::client' :
     package_ensure => 'present'
   }
-  mysql::db { 'sonar':
-      user     => 'sonar',
-      password => 'sonar',
-      host     => '%.%.%.%',
-      grant    => ['ALL'],
-  }
-  mysql::db { 'gitlabhq_production':
-      user     => 'gitlab',
-      password => 'password',
-      host     => '%.%.%.%',
-      grant    => ['ALL'],
-      charset  => 'utf8',
-      collate  => 'utf8_unicode_ci'
-  }
-  mysql_grant { 'gitlab@%.%.%.%': 
-    ensure => 'present',
-    privileges => ['ALL'],
-    table      => 'gitlab.*',
-    user       => 'gitlab@%.%.%.%',
-  }
-  mysql_grant { 'sonar@%.%.%.%': 
-    ensure => 'present',
-    privileges => ['ALL'],
-    table      => 'sonar.*',
-    user       => 'sonar@%.%.%.%',
-  }
-  file { ['/opt/gitlab', '/opt/gitlab/data'] :
-    ensure  => 'directory',
-  }->
-  exec { 'docker-gitlab-firstrun' :
-    require => File['/opt/gitlab'],
-    command => "docker run --name=gitlab -i -t --rm -e \"DB_HOST=${ipaddress_eth0}\" -e \"DB_NAME=gitlabhq_production\" -e \"DB_USER=gitlab\" -e \"DB_PASS=password\" -v /opt/gitlab/data:/home/git/data sameersbn/gitlab:7.0.0 app:rake gitlab:setup force=yes",
-    onlyif => "test -z `docker ps -a | grep gitlab`",
-    timeout => 0
-  }->
-  exec { 'docker-normal-run' :
-    onlyif => "test -n `docker ps -a | grep gitlab`",
-    command => "docker run --name=gitlab -d -e \"DB_HOST=${ipaddress_eth0}\" -e \"DB_NAME=gitlabhq_production\" -e \"DB_USER=gitlab\" -e \"DB_PASS=password\" -e \"GITLAB_PORT=10080\" -e \"GITLAB_SSH_PORT=10022\" -e \"GITLAB_RELATIVE_URL_ROOT=/gitlab\" -p 127.0.0.1:10022:22 -p 127.0.0.1:10080:80 -v /opt/gitlab/data:/home/git/data sameersbn/gitlab:7.0.0",
-    timeout => 0
-  }
+  include profiles::gitlab
   class { '::java' : 
   }
   include profiles::jenkinsmaster
@@ -73,36 +34,6 @@ node 'ci-master' {
     nexus_root => '/opt'
   }
   include profiles::sonar
-#  sonarqube::plugin { 'sonar-build-breaker-plugin' :
-#    groupid    => 'org.codehaus.sonar-plugins',
-#    artifactid => 'sonar-build-breaker-plugin',
-#    version    => '1.1',
-#    require  => Class['maven::maven'],
-#  }
-#  sonarqube::plugin { 'sonar-build-stability-plugin' :
-#    groupid    => 'org.codehaus.sonar-plugins',
-#    artifactid => 'sonar-build-stability-plugin',
-#    version    => '1.2',
-#    require  => Class['maven::maven'],
-#  }
-#  sonarqube::plugin { 'sonar-groovy-plugin' :
-#    groupid    => 'org.codehaus.sonar-plugins',
-#    artifactid => 'sonar-groovy-plugin',
-#    version    => '1.0.1',
-#    require  => Class['maven::maven'],
-#  }
-#  sonarqube::plugin { 'sonar-javascript-plugin' :
-#    groupid    => 'org.codehaus.sonar-plugins.javascript',
-#    artifactid => 'sonar-javascript-plugin',
-#    version    => '1.6',
-#    require  => Class['maven::maven'],
-#  }
-#  sonarqube::plugin { 'sonar-branding-plugin' :
-#    groupid    => 'org.codehaus.sonar-plugins',
-#    artifactid => 'sonar-branding-plugin',
-#    version    => '0.4',
-#    require  => Class['maven::maven'],
-#  }
   class { 'gradle':
     version => '1.12',
     require => Package['unzip']
